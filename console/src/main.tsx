151d311d6062fe3cc3aaa71ec31d9860cbaa532e
Hayden Young <hyoung@acquired.com>
2026-02-21 12:54:10 +0000

    feat: init repo projects

---
+++ console/src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import { TooltipProvider } from './components/ui/tooltip'
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <>
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
+    </>
+  );
+}
