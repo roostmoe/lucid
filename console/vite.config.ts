39fcceee97857298bfcd6d42388ae996c8d06c6b
Hayden Young <hyoung@acquired.com>
2026-02-21 16:07:58 +0000

    fix: for now, add a dummy test
    There's nothing built in the console yet, so this is just to shut up the
    CI job for console tests

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -13,6 +13,10 @@ const config = defineConfig({
 		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		viteReact(),
 	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
 });
 
 export default config;
